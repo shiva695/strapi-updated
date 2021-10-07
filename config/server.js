4879517f89388dcef67e1daaf51e1cce4b24963c
Abishek J Reuben <abishekjreuben@gmail.com>
2021-10-07 12:25:08 +0530

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '513c9b2778e480ea22aee672a029bf12'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0b5c6971-558d-4f02-b41b-8e0c7a6be2ee'
+};
