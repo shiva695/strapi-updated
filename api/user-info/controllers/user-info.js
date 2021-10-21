3f6853b5f462ffa6ffb83c3c3da6dceee7d3bd30
Abishek J Reuben <abishekjreuben@gmail.com>
2021-10-21 13:42:47 +0530

    Added APIs

--- api/user-info/controllers/user-info.js
+++ api/user-info/controllers/user-info.js
@@ -1,8 +1,74 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+ const axios = require("axios");
 
-module.exports = {};
+ let api = axios.create({ baseURL: "http://localhost:1337" });
+
+module.exports = {
+    async create( ctx )
+    {
+        let { name, email, password, mobile } = ctx.request.body;
+
+        let response = {};
+
+        let user = await strapi.query( 'user', 'users-permissions' ).findOne( {username: name } );
+
+        if ( name != null && email != null && password != null && mobile != null && user == null)
+        {
+            let userRegisterReqBody = {
+                username: name,
+                email: email,
+                password: password
+            };
+            let initSignup = await api.post(
+                "/auth/local/register",
+                userRegisterReqBody
+            );
+
+            let createUserInformationReqBody = {
+                ParentName: name,
+                Email: email,
+                Mobile: mobile
+              };
+            await strapi.query("user-info").create(createUserInformationReqBody);
+            response.userinformation = createUserInformationReqBody;
+
+            response.status = "User registered..";
+            response.accesstoken = initSignup.data.jwt;
+            response.userid = initSignup.data.user._id;
+            response.username = initSignup.data.user.username;
+            response.email = initSignup.data.user.email;
+            return response;
+        }
+        else
+        {
+            if (email != null && password != null && user != null )
+            {
+                let userLoginReqBody = {
+                    identifier: email,
+                    password: password,
+                  };
+
+                let initLogin = await api.post("/auth/local", userLoginReqBody);
+                response.status = "User exists..";
+                response.accesstoken = initLogin.data.jwt;
+                response.userid = initLogin.data.user._id;
+                response.username = initLogin.data.user.username;
+                response.email = initLogin.data.user.email;
+
+                let userInformation = await strapi.query( "user-info" ).findOne( { Email: email } );
+                response.userinformation = userInformation
+
+                let children = await strapi.query( 'child' ).find( { parentEmail: email, _limit: 10 } );
+
+                for (var i in children )
+                {
+                    let reports = await strapi.query( "report" ).find( { ChildID: children[i].ChildID } );
+                    children[i].reports = reports;
+                }
+                response.children = children;
+                return response;
+            }    
+        }
+    }
+};
--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int( 'PORT', 1337 ),
-  url: 'https://cms.svassessments.fxgetactive.com',
+  //url: 'https://cms.svassessments.fxgetactive.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '513c9b2778e480ea22aee672a029bf12'),
